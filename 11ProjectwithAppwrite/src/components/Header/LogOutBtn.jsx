1e1d15c9ce092cf573a2d6a75ea421d80e4caf59
Anuj Kumar <personeluse2000@gmail.com>
2026-03-27 22:31:24 +0530

    some changes

--- 11ProjectwithAppwrite/src/components/Header/LogOutBtn.jsx
+++ 11ProjectwithAppwrite/src/components/Header/LogOutBtn.jsx
@@ -4,17 +4,23 @@ import authservice from '../../appwrite/aut_service'
 import {logout} from '../../store/authSlice'
 const LogOutBtn = () => {
 
-    const disptach = useDispatch()
+    const dispatch  = useDispatch()
+
+// Pehle:
+// Backend logout ho raha hai
+// 👉 Fir:
+// Redux ko bola ja raha hai:
+// ➜ "User ab logout ho gaya hai, state update karo"
 
     const logoutHandler = () => {
         authservice.logout().
         then(() => {
-            disptach(logout())
+            dispatch (logout())
         } )
     }
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
     onClick={logoutHandler}
     >Logout</button>
   )
