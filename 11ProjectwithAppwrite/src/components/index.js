040ddbb0c437bd051a0aaed61d7cb1ea4b14d9e8
Anuj Kumar <personeluse2000@gmail.com>
2026-03-30 01:17:50 +0530

    some new featyres has being added

--- 11ProjectwithAppwrite/src/components/index.js
+++ 11ProjectwithAppwrite/src/components/index.js
@@ -3,11 +3,17 @@ import Footer from "./Footer/Footer";
 import Container from "../container/Container";
 import Logo from "./Logo";
 import LogOutBtn from "./Header/LogOutBtn";
+import Select from "./Select"
+import Input from './Input'
+import Button from './Button'
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogOutBtn
+    LogOutBtn,
+    Button,
+    Input,
+    Select
 }
